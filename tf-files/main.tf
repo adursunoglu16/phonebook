091f9abafc1a9ec13b6c77f56d2b58fce815846f
adursunoglu16 <adursunoglu16@gmail.com>
2021-12-07 21:24:55 +0300

    updated

--- tf-files/main.tf
+++ tf-files/main.tf
@@ -36,7 +36,7 @@ resource "aws_alb_target_group" "app-lb-tg" {
   name = "phonebook-lb-tg"
   port = 80
   protocol = "HTTP"
-  vpc_id = "vpc-0328947e"
+  vpc_id = "vpc-0328947e" //data.aws_vpc.selected.id
   target_type = "instance"
   health_check {
     healthy_threshold = 2
@@ -50,6 +50,7 @@ resource "aws_alb" "app-lb" {
   load_balancer_type = "application"
   security_groups = [aws_security_group.alb-sg.id]
   subnets = ["subnet-847bccb5", "subnet-e28de3c3", "subnet-9b3b58fd","subnet-3627147b","subnet-566a0609","subnet-b3795cbd"]
+            //data.aws_subnets.example.ids
 }
 resource "aws_alb_listener" "app-listener" {
     load_balancer_arn = aws_alb.app-lb.arn
@@ -82,21 +83,25 @@ resource "aws_db_instance" "db-server" {
   auto_minor_version_upgrade = true
   backup_retention_period = 0 
   identifier = "phonebook-app-db"
-  name = "phonebook"
+  name = "phonebook"  // must be same with db values in the phonebook-app.py
   engine = "mysql"
   engine_version = "8.0.20"
-  username = "admin"
-  password = "Dursun_1"
+  username = "admin" // must be same with db values in the phonebook-app.py
+  password = "Dursun_1" // must be same with db values in the phonebook-app.py
   multi_az = false
-  port = 3306
+  port = 3306  // must be same with db values in the phonebook-app.py
   publicly_accessible = false
-  skip_final_snapshot = true
+  skip_final_snapshot = true //not taking final snapshot when we terminate
   monitoring_interval = 0
 }
 resource "github_repository_file" "dbendpoint" {
   content = aws_db_instance.db-server.address
-  file = "dbserver.endpoint"
-  repository = "phonebook"
+  file = "dbserver.endpoint"  // must be same with db values in the phonebook-app.py
+  repository = "phonebook"    // must be same with my repo for this project
   overwrite_on_create = true
-  branch = "master"
+  branch = "master"      // check your repo 'master' or 'main'
 }
+
+// burada oncelikle terraform apply deyince rds icin bir dbendpoint oluscak ve o github repoya gitcek. 
+//ec2lar olusurken launch template phonebook-app.py kullanarak bu db endpoint ile ec2lari iletisime gecircek.
+//Launch template e bakarsan zaten github i depends on yaptik yani once github file oluscak sonra lt harekete geccek
